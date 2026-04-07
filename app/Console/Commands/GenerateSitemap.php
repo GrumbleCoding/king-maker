4fc2cc5bb6988544dc641a5237ecdeb53f10fca1
Shubham <shubham.bhadoriya@imperoit.com>
2026-04-07 15:42:00 +0530

    search on google

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+use Illuminate\Console\Command;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Sitemap::create()
+            ->add(Url::create('/'))
+            ->add(Url::create('/about'))
+            ->add(Url::create('/contact'))
+            // Add other URLs or dynamically from database
+            ->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generated successfully!');
+    }
+}
